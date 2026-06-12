74ba2346149f0e92ce5d26f95256bb94fe844535
Artyom <art161994@yandex.ru>
2026-06-12 13:51:09 +0200

    Update product page and product card, add Loader and Error component, update styles

---
+++ react-redux-project/src/components/products/ProductCard.jsx
@@ -0,0 +1,21 @@
+function ProductCard({ product }) {
+  return (
+    <article className="product-card">
+      <img
+        className="product-card__image"
+        src={product.image}
+        alt={product.title}
+      />
+
+      <div className="product-card__content">
+        <h3 className="product-card__title">{product.title}</h3>
+
+        <p className="product-card__category">{product.category}</p>
+
+        <p className="product-card__price">${product.price}</p>
+      </div>
+    </article>
+  );
+}
+
+export default ProductCard;
--- react-redux-project/src/pages/ProductsPage.jsx
+++ react-redux-project/src/pages/ProductsPage.jsx
@@ -1,8 +1,49 @@
+import ErrorMessage from "../components/ErrorMessage";
+import Loader from "../components/Loader";
+import ProductList from "../components/products/ProductList";
+
+const products = [
+  {
+    id: 1,
+    title: 'Backpack',
+    price: 49.99,
+    category: 'bags',
+    image: 'https://placehold.co/300x300?text=Backpack',
+  },
+  {
+    id: 2,
+    title: 'T-Shirt',
+    price: 19.99,
+    category: 'clothes',
+    image: 'https://placehold.co/300x300?text=T-Shirt',
+  },
+  {
+    id: 3,
+    title: 'Sneakers',
+    price: 89.99,
+    category: 'shoes',
+    image: 'https://placehold.co/300x300?text=Sneakers',
+  },
+];
+
 function ProductsPage() {
+  const isLoading = false;
+  const error = null;
+
+  if (isLoading) {
+    return <Loader/>;
+  }
+
+  if (error) {
+    return <ErrorMessage message= 'Failed to load products'/>;
+  }
+
   return (
     <section>
       <h1>Products Page</h1>
       <p>Here we will display products from an external API.</p>
+
+      <ProductList products={products}/>
     </section>
   );
 }
