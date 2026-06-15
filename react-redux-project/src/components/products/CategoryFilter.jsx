1059104f5cca5f0e7a1328784a31cc766f5755a4
Artyom <art161994@yandex.ru>
2026-06-15 20:55:49 +0200

    Add product category filter

---
+++ react-redux-project/src/components/products/CategoryFilter.jsx
@@ -0,0 +1,34 @@
+function CategoryFilter({ categories, selectedCategory, onCategoryChange }) {
+  return (
+    <div className="category-filter">
+      <button
+        className={
+          selectedCategory === 'all'
+            ? 'category-filter__button category-filter__button--active'
+            : 'category-filter__button'
+        }
+        type="button"
+        onClick={() => onCategoryChange('all')}
+      >
+        All
+      </button>
+
+      {categories.map((category) => (
+        <button
+          className={
+            selectedCategory === category
+              ? 'category-filter__button category-filter__button--active'
+              : 'category-filter__button'
+          }
+          key={category}
+          type="button"
+          onClick={() => onCategoryChange(category)}
+        >
+          {category}
+        </button>
+      ))}
+    </div>
+  );
+}
+
+export default CategoryFilter;
--- react-redux-project/src/pages/ProductsPage.jsx
+++ react-redux-project/src/pages/ProductsPage.jsx
@@ -1,17 +1,31 @@
-import { useEffect } from 'react';
+import { useEffect, useMemo, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 
 import ErrorMessage from '../components/ErrorMessage';
 import Loader from '../components/Loader';
+import CategoryFilter from '../components/products/CategoryFilter';
 import ProductList from '../components/products/ProductList';
 import { fetchProducts } from '../features/products/productsSlice';
 
 function ProductsPage() {
   const dispatch = useDispatch();
+  const [selectedCategory, setSelectedCategory] = useState('all');
 
   const products = useSelector((state) => state.products.products);
   const isLoading = useSelector((state) => state.products.isLoading);
   const error = useSelector((state) => state.products.error);
+
+  const categories = useMemo(() => {
+    return [...new Set(products.map((product) => product.category))];
+  }, [products]);
+
+  const filteredProducts = useMemo(() => {
+    if (selectedCategory === 'all') {
+      return products;
+    }
+
+    return products.filter((product) => product.category === selectedCategory);
+  }, [products, selectedCategory]);
 
   const handleRetry = () => {
     dispatch(fetchProducts());
@@ -35,7 +49,19 @@ function ProductsPage() {
       <p>Products are loaded from an external API.</p>
 
       {products.length > 0 ? (
-        <ProductList products={products} />
+        <>
+          <CategoryFilter
+            categories={categories}
+            selectedCategory={selectedCategory}
+            onCategoryChange={setSelectedCategory}
+          />
+
+          {filteredProducts.length > 0 ? (
+            <ProductList products={filteredProducts} />
+          ) : (
+            <p>No products found in this category.</p>
+          )}
+        </>
       ) : (
         <p>No products found.</p>
       )}
